ee8fc9475873afbe06de94befb996df9ad942844
Marc Plans <marcplansr@gmail.com>
2019-08-16 10:39:49 +0200

    Deleted obsolete sql files

--- src/main/java/com/videorental/utilities/NotFoundException.java
+++ src/main/java/com/videorental/utilities/NotFoundException.java
@@ -3,7 +3,7 @@ package com.videorental.utilities;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "El parametro introducido no existe.")
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "El parámetro introducido no existe.")
 public class NotFoundException extends Exception {
 
 	// Generated UID
